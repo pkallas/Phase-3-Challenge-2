44f430aa9db60b8cc8dd4b2878be71824eae967d
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-09-05 17:17:32 -0700

    Finished tests, all passing

--- Part_2/test/database_test.js
+++ Part_2/test/database_test.js
@@ -88,3 +88,98 @@ describe('function availableRooms', function () {
     });
   });
 });
+
+describe('function bookings', function () {
+  it('Should return all rooms currently booked from the bookings table', function () {
+    return bookings(bookingsSelect)
+    .then(res => {
+      expect(res.length).to.eql(35);
+    });
+  });
+
+  it('Should only return rooms after the current date', function () {
+    return bookings(bookingsSelect)
+    .then(res => {
+      expect(res[0].check_out).to.be.above(new Date());
+    });
+  });
+
+  it('Should only return rooms after the current date', function () {
+    return bookings(bookingsSelect)
+    .then(res => {
+      expect(res[2].check_out).to.be.above(new Date());
+    });
+  });
+
+  it('Should only return rooms after the current date', function () {
+    return bookings(bookingsSelect)
+    .then(res => {
+      expect(res[30].check_out).to.be.above(new Date());
+    });
+  });
+
+  it('Should only return rooms after the current date', function () {
+    return bookings(bookingsSelect)
+    .then(res => {
+      expect(res[23].check_out).to.be.above(new Date());
+    });
+  });
+
+  it('Should only return rooms after the current date', function () {
+    return bookings(bookingsSelect)
+    .then(res => {
+      expect(res[34].check_out).to.be.above(new Date());
+    });
+  });
+});
+
+describe('function bookingsForRoom', function () {
+  it('Should return all bookings for 3B', function () {
+    return bookingsForRoom(bookingsForRoomSelect, '3B')
+    .then(res => {
+      expect(res.length).to.eql(3);
+    });
+  });
+
+  it('Should return all bookings for 3B', function () {
+    return bookingsForRoom(bookingsForRoomSelect, '3B')
+    .then(res => {
+      res.forEach(index => expect(index.room_number).to.eql('3B'));
+    });
+  });
+
+  it('Should return all bookings for 4A', function () {
+    return bookingsForRoom(bookingsForRoomSelect, '4A')
+    .then(res => {
+      res.forEach(index => expect(index.room_number).to.eql('4A'));
+    });
+  });
+
+  it('Should return all bookings for 3E', function () {
+    return bookingsForRoom(bookingsForRoomSelect, '3E')
+    .then(res => {
+      res.forEach(index => expect(index.room_number).to.eql('3E'));
+    });
+  });
+
+  it('Should return all bookings for 5B', function () {
+    return bookingsForRoom(bookingsForRoomSelect, '5B')
+    .then(res => {
+      res.forEach(index => expect(index.room_number).to.eql('5B'));
+    });
+  });
+
+  it('Should return all bookings for 2A', function () {
+    return bookingsForRoom(bookingsForRoomSelect, '2A')
+    .then(res => {
+      res.forEach(index => expect(index.room_number).to.eql('2A'));
+    });
+  });
+
+  it('Should return all bookings for 2A', function () {
+    return bookingsForRoom(bookingsForRoomSelect, '2A')
+    .then(res => {
+      expect(res.length).to.eql(2);
+    });
+  });
+});
