5e2b6a8e38d4f39729f4c978f791e65280899f7a
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-09-08 16:34:48 -0700

    Moved sequel statements into functions themselves, moved csv files into folder

--- Part_2/database.js
+++ Part_2/database.js
@@ -1,31 +1,39 @@
 const client = require('./pg');
 
-const guests = function (text) {
-  return client.query(text)
+const guests = function () {
+  return client.query('SELECT * FROM guests')
   .then(res => res.rows)
   .catch(error => console.log(error));
 };
 
-const rooms = function (text) {
-  return client.query(text)
+const rooms = function () {
+  return client.query('SELECT room_number, capacity, available FROM rooms')
   .then(res => res.rows)
   .catch(error => console.log(error));
 };
 
-const availableRooms = function (text) {
-  return client.query(text)
+const availableRooms = function () {
+  return client.query(`SELECT room_number, capacity, available FROM rooms
+  WHERE available = true`)
   .then(res => res.rows)
   .catch(error => console.log(error));
 };
 
-const bookings = function (text) {
-  return client.query(text)
+const bookings = function () {
+  return client.query(`SELECT room_number, name, check_in, check_out FROM bookings
+  JOIN rooms ON rooms.id = bookings.room_id
+  JOIN guests ON guests.id = bookings.guest_id
+  WHERE check_out > CURRENT_DATE`)
   .then(res => res.rows)
   .catch(error => console.log(error));
 };
 
-const bookingsForRoom = function (text, roomNumber) {
-  return client.query(text, [roomNumber])
+const bookingsForRoom = function (roomNumber) {
+  return client.query(`SELECT room_number, name, check_in, check_out FROM bookings
+  JOIN rooms ON rooms.id = bookings.room_id
+  JOIN guests ON guests.id = bookings.guest_id
+  WHERE check_out > CURRENT_DATE
+  AND room_number = $1`, [roomNumber])
   .then(res => res.rows)
   .catch(error => console.log(error));
 };
--- Part_2/test/database_test.js
+++ Part_2/test/database_test.js
@@ -7,31 +7,24 @@ const {
   bookings,
   bookingsForRoom,
 } = require('../database');
-const {
-  guestsSelect,
-  roomsSelect,
-  availableRoomsSelect,
-  bookingsSelect,
-  bookingsForRoomSelect,
-} = require('../sqlstatements');
 
 describe('function guests', function () {
   it('Should return all guests from the guests table', function () {
-    return guests(guestsSelect)
+    return guests()
     .then(res => {
       expect(res.length).to.eql(20);
     });
   });
 
   it('Should return Aurthur Velti as the first guest', function () {
-    return guests(guestsSelect)
+    return guests()
     .then(res => {
       expect(res[0].name).to.eql('Aurthur Velti');
     });
   });
 
   it('Should return Penelope Searchfield as the last guest', function () {
-    return guests(guestsSelect)
+    return guests()
     .then(res => {
       expect(res[19].name).to.eql('Penelope Searchfield');
     });
@@ -40,28 +33,28 @@ describe('function guests', function () {
 
 describe('function rooms', function () {
   it('Should return all room numbers, their capacity, and their availability from the rooms table', function () {
-    return rooms(roomsSelect)
+    return rooms()
     .then(res => {
       expect(res.length).to.eql(18);
     });
   });
 
   it('Should return room 2A as the first room', function () {
-    return rooms(roomsSelect)
+    return rooms()
     .then(res => {
       expect(res[0].room_number).to.eql('2A');
     });
   });
 
   it('Should return room 5A as the last room', function () {
-    return rooms(roomsSelect)
+    return rooms()
     .then(res => {
       expect(res[17].room_number).to.eql('5A');
     });
   });
 
   it('Should return a room even if it is not available', function () {
-    return rooms(roomsSelect)
+    return rooms()
     .then(res => {
       expect(res[17].available).to.eql(false);
     });
@@ -70,28 +63,28 @@ describe('function rooms', function () {
 
 describe('function availableRooms', function () {
   it('Should return all room numbers and their capacity if they are available', function () {
-    return availableRooms(availableRoomsSelect)
+    return availableRooms()
     .then(res => {
       expect(res.length).to.eql(16);
     });
   });
 
   it('Should only return rooms if they available', function () {
-    return availableRooms(availableRoomsSelect)
+    return availableRooms()
     .then(res => {
       expect(res[0].available).to.eql(true);
     });
   });
 
   it('Should only return rooms if they available', function () {
-    return availableRooms(availableRoomsSelect)
+    return availableRooms()
     .then(res => {
       expect(res[3].available).to.eql(true);
     });
   });
 
   it('Should only return rooms if they available', function () {
-    return availableRooms(availableRoomsSelect)
+    return availableRooms()
     .then(res => {
       expect(res[15].available).to.eql(true);
     });
@@ -100,42 +93,42 @@ describe('function availableRooms', function () {
 
 describe('function bookings', function () {
   it('Should return all rooms currently booked from the bookings table', function () {
-    return bookings(bookingsSelect)
+    return bookings()
     .then(res => {
       expect(res.length).to.eql(35);
     });
   });
 
   it('Should only return rooms after the current date', function () {
-    return bookings(bookingsSelect)
+    return bookings()
     .then(res => {
       expect(res[0].check_out).to.be.above(new Date());
     });
   });
 
   it('Should only return rooms after the current date', function () {
-    return bookings(bookingsSelect)
+    return bookings()
     .then(res => {
       expect(res[2].check_out).to.be.above(new Date());
     });
   });
 
   it('Should only return rooms after the current date', function () {
-    return bookings(bookingsSelect)
+    return bookings()
     .then(res => {
       expect(res[30].check_out).to.be.above(new Date());
     });
   });
 
   it('Should only return rooms after the current date', function () {
-    return bookings(bookingsSelect)
+    return bookings()
     .then(res => {
       expect(res[23].check_out).to.be.above(new Date());
     });
   });
 
   it('Should only return rooms after the current date', function () {
-    return bookings(bookingsSelect)
+    return bookings()
     .then(res => {
       expect(res[34].check_out).to.be.above(new Date());
     });
@@ -144,49 +137,49 @@ describe('function bookings', function () {
 
 describe('function bookingsForRoom', function () {
   it('Should return all bookings for 3B', function () {
-    return bookingsForRoom(bookingsForRoomSelect, '3B')
+    return bookingsForRoom('3B')
     .then(res => {
       expect(res.length).to.eql(3);
     });
   });
 
   it('Should return all bookings for 3B', function () {
-    return bookingsForRoom(bookingsForRoomSelect, '3B')
+    return bookingsForRoom('3B')
     .then(res => {
       res.forEach(index => expect(index.room_number).to.eql('3B'));
     });
   });
 
   it('Should return all bookings for 4A', function () {
-    bookingsForRoom(bookingsForRoomSelect, '4A')
+    bookingsForRoom('4A')
     .then(res => {
       res.forEach(index => expect(index.room_number).to.eql('4B'));
     });
   });
 
   it('Should return all bookings for 3E', function () {
-    return bookingsForRoom(bookingsForRoomSelect, '3E')
+    return bookingsForRoom('3E')
     .then(res => {
       res.forEach(index => expect(index.room_number).to.eql('3E'));
     });
   });
 
   it('Should return all bookings for 5B', function () {
-    return bookingsForRoom(bookingsForRoomSelect, '5B')
+    return bookingsForRoom('5B')
     .then(res => {
       res.forEach(index => expect(index.room_number).to.eql('5B'));
     });
   });
 
   it('Should return all bookings for 2A', function () {
-    return bookingsForRoom(bookingsForRoomSelect, '2A')
+    return bookingsForRoom('2A')
     .then(res => {
       res.forEach(index => expect(index.room_number).to.eql('2A'));
     });
   });
 
   it('Should return all bookings for 2A', function () {
-    return bookingsForRoom(bookingsForRoomSelect, '2A')
+    return bookingsForRoom('2A')
     .then(res => {
       expect(res.length).to.eql(2);
     });
