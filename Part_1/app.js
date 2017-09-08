67b358ff62fce8e4d63791b9b9c4c8a2ef1d15ae
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-09-08 16:18:51 -0700

    Refactored express post route in part 1 to be less repetitive

--- Part_1/app.js
+++ Part_1/app.js
@@ -4,43 +4,28 @@ const bodyParser = require('body-parser');
 
 app.listen(3000, () => console.log('Listening at port 3000'));
 
-app.get('/api/shout/:word', (request, response) => {
-  response.send(`${request.params.word.toUpperCase()}!!!`);
+app.get('/api/shout/:w', (request, response) => {
+  response.send(`${request.params.w.toUpperCase()}!!!`);
 });
 
 app.use(bodyParser.json());
 
 app.post('/api/array/merge', (request, response) => {
-  let arrayA = request.body['a'];
-  let arrayB = request.body['b'];
-  if (Array.isArray(arrayA) === false) {
-    response.status(400).json({"error": "Input data should be of type Array."})
-  } else if (Array.isArray(arrayB) === false) {
-    response.status(400).json({"error": "Input data should be of type Array."})
+  let arrayA = request.body.a;
+  let arrayB = request.body.b;
+  let longestArray;
+  if (Array.isArray(arrayA) === false || Array.isArray(arrayB) === false) {
+    response.status(400).json({ "error": "Input data should be of type Array." });
   } else {
     let mergedArray = [];
-    if (arrayA.length > arrayB.length) {
-      let index = 0;
-      do {
-        mergedArray.push(arrayA[index]);
-        mergedArray.push(arrayB[index]);
-        index++;
-      } while (index < arrayA.length);
-    } else if (arrayB.length > arrayA.length) {
-      let index = 0;
-      do {
-        mergedArray.push(arrayA[index]);
-        mergedArray.push(arrayB[index]);
-        index++;
-      } while (index < arrayB.length);
-    } else {
-      let index = 0;
-      do {
-        mergedArray.push(arrayA[index]);
-        mergedArray.push(arrayB[index]);
-        index++;
-      } while (index < arrayA.length);
-    }
-    response.json({ "result": mergedArray });
+    arrayA.length > arrayB.length ? longestArray = arrayA : longestArray = arrayB;
+    let index = 0;
+    do {
+      mergedArray.push(arrayA[index]);
+      mergedArray.push(arrayB[index]);
+      index++;
+    } while (index < longestArray.length);
+    let finalArray = mergedArray.filter(index => index !== undefined);
+    response.json({ "result": finalArray });
   };
 });
